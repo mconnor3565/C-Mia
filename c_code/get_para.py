10b3e5cbe183279869ce42612671f57236e2408a
mconnor3565 <miaconnor2828@gmail.com>
2024-04-22 20:32:09 -0400

    update

--- c_code/get_para.py
+++ c_code/get_para.py
@@ -1,8 +1,8 @@
 import numpy as np 
 from scipy.optimize import curve_fit
 import matplotlib.pyplot as plt
-#calculating translational displacement 
 
+#calculating translational displacement 
 t,dx,dy,dz = np.loadtxt("CoM_displacement.dat", unpack=True, usecols=(0,1,2,3))
 
 inital_pos = np.array([dx[0], dy[0], dz[0]])
@@ -25,32 +25,26 @@ with open("data/h_data.txt", 'w') as f:
     for i in range(len(t)):
         e_1 = np.array([e1x[i], e1y[i], e1z[i]])
         e_2 = np.array([e2x[i], e2y[i], e2z[i]])
-        # numerator = np.subtract(e_2, e_1)
         numerator = e_2 - e_1
         denominator = np.sqrt(np.dot(numerator, numerator))
         print('d=', denominator)
-        # denominator = 1
-        # print(t, "n=", numerator, 'd=' , denominator)
         h = numerator / denominator
-        head_vector[i,0]= h[0]
-        head_vector[i,1]= h[1]
-        head_vector[i,2]= h[2]
+        f_vector[i,0]= h[0]
+        f_vector[i,1]= h[1]
+        f_vector[i,2]= h[2]
         f.write("{} {:.4f} {:.4f} {:.4f}\n".format(t[i], f_vector[i,0], f_vector[i,1], f_vector[i,2]))
 
-#getting the parallel displacement and fitting  
+#getting the parallel displacement   
 disp_para = np.zeros((len(t),3))
 with open('data/disp_para.txt', 'w') as f:
     f.write('#t disp para\n')
     for i in range(len(t)):
         f_vector = np.array([f_vector[i,0], f_vector[i,1], f_vector[i,2]])
         d = np.array([disp_vector[i,0], disp_vector[i,1], disp_vector[i,2]])
-        p = np.dot(head, d)
+        p = np.dot(f_vector, d)
         f.write("{} {} {}\n".format(t[i], p, p**2)) 
 
-def f(x, m, c):
-    return m*x + c
-
-
+#windowing average 
 t, p, p2 = np.loadtxt('data/disp_para.txt', unpack=True)
 
 t_max = len(t)
@@ -64,19 +58,22 @@ t0_iter_list = np.arange(0, t_max-sample_window)
 print("Samples taken: {}".format(len(t0_iter_list)))
 
 t_shortened = t[:sample_window]
-
 for t0_i in t0_iter_list:
-    init_angle = p[t0_i]
-    p_sq_avg_sampled+= (p[t0_i:t0_i+sample_window]-init_angle)**2
+    init_p = p[t0_i]
+    p_sq_avg_sampled+= (p[t0_i:t0_i+sample_window]-init_p)**2
+
+#getting D parallel 
+def f(x, m, c):
+    return m*x + c
 
 p_sq_avg_sampled /= len(t0_iter_list)
 Dimension = 1
-
 fit_params, fit_cov = curve_fit(f, t_shortened, p_sq_avg_sampled)
 print('Fitted parameters: m = {:.4e}, c = {:.4e}'.format(fit_params[0], fit_params[1]))
 print('Diffusion coefficient: D = {:.4e}'.format(fit_params[0]/(2*Dimension)))
 print('or, approximately D = {:.4f}'.format(fit_params[0]/(2*Dimension)))
 
+D_para = fit_params[0]/(2*Dimension)
 fitline = f(t_shortened, *fit_params)
 
 plt.figure(tight_layout=True)
@@ -86,6 +83,22 @@ plt.xlabel(r'$t/\tau$', fontsize=18)
 plt.ylabel(r'$\langle MSD_{parallel}^2 \rangle$', fontsize=18)
 plt.title(r"$D_{{para}} = {:.4f}$".format(fit_params[0]/(2*Dimension)))
 plt.legend(fontsize=14)
-plt.savefig('plots/D_paralle_cl.pdf')
+plt.savefig('plots/D_parallel_c.pdf')
 
+#getting D total
+t, ds_sq = np.loadtxt("window_displacement.dat", unpack=True)
+
+Dimension= 3
+
+def f(x, m, c):
+    return m * x + c
+
+popt, pcov = curve_fit(f, t, ds_sq)
+
+D_measured = popt[0] / (2.0*Dimension)
+
+ 
+print(' D perp = {:.4f}'.format(D_measured - D_para))
+
+ 
 
--- lammps_sim/calc_dist.py
+++ lammps_sim/calc_dist.py
@@ -2,11 +2,10 @@ import numpy as np
 from scipy.optimize import curve_fit
 import matplotlib.pyplot as plt
 
+#calculating translational displacement 
 t,dx,dy,dz = np.loadtxt("data/com_pos.txt", unpack=True, usecols=(0,1,2,3))
 
 inital_pos = np.array([dx[0], dy[0], dz[0]])
-
-
 disp_vector = np.zeros((len(t),3))
 with open("dis_data.txt", 'w') as f:
     f.write('#t dx dy dz\n')
@@ -18,6 +17,7 @@ with open("dis_data.txt", 'w') as f:
         disp_vector[i,2]= d[2]
         f.write("{} {:.4f} {:.4f} {:.4f}\n".format(t[i], disp_vector[i,0], disp_vector[i,1], disp_vector[i,2]))
 
+#getting  the head vector 
 t,e1x, e1y, e1z, e2x, e2y, e2z = np.loadtxt("data/e2e_pos.txt", unpack=True)
 
 head_vector = np.zeros((len(t),3))
@@ -38,6 +38,7 @@ with open("data/h_data.txt", 'w') as f:
         head_vector[i,2]= h[2]
         f.write("{} {:.4f} {:.4f} {:.4f}\n".format(t[i], head_vector[i,0], head_vector[i,1], head_vector[i,2]))
 
+#getting the parallel displacement   
 disp_para = np.zeros((len(t),3))
 with open('data/disp_para.txt', 'w') as f:
     f.write('#t disp para\n')
@@ -47,10 +48,8 @@ with open('data/disp_para.txt', 'w') as f:
         p = np.dot(head, d)
         f.write("{} {} {}\n".format(t[i], p, p**2))
 
-def f(x, m, c):
-    return m*x + c
 
-
+#cacluating D para
 t, p, p2 = np.loadtxt('data/disp_para.txt', unpack=True)
 
 t_max = len(t)
@@ -64,18 +63,21 @@ t0_iter_list = np.arange(0, t_max-sample_window)
 print("Samples taken: {}".format(len(t0_iter_list)))
 
 t_shortened = t[:sample_window]
-
 for t0_i in t0_iter_list:
     init_angle = p[t0_i]
     p_sq_avg_sampled+= (p[t0_i:t0_i+sample_window]-init_angle)**2
 
+def f(x, m, c):
+    return m*x + c
+
 p_sq_avg_sampled /= len(t0_iter_list)
 Dimension = 1
-
 fit_params, fit_cov = curve_fit(f, t_shortened, p_sq_avg_sampled)
 print('Fitted parameters: m = {:.4e}, c = {:.4e}'.format(fit_params[0], fit_params[1]))
 print('Diffusion coefficient: D = {:.4e}'.format(fit_params[0]/(2*Dimension)))
 print('or, approximately D = {:.4f}'.format(fit_params[0]/(2*Dimension)))
+
+D_para = fit_params[0]/(2*Dimension) 
 
 fitline = f(t_shortened, *fit_params)
 
@@ -88,8 +90,7 @@ plt.title(r"$D_{{para}} = {:.4f}$".format(fit_params[0]/(2*Dimension)))
 plt.legend(fontsize=14)
 plt.savefig('plots/D_parallel_l.pdf')
 
-
-
+#caclulating D total 
 t,dx,dy,dz = np.loadtxt("data/dis_data.txt", unpack=True, usecols=(0,1,2,3))
 
 t_max = len(t)
@@ -107,15 +108,13 @@ for t0_i in t0_iter_list:
     init_z = dz[t0_i]
     disp_sq_avg_sampled += (dx[t0_i:t0_i+sampling_window] - init_x)**2 + (dy[t0_i:t0_i+sampling_window] - init_y)**2 + (dz[t0_i:t0_i+sampling_window] - init_z)**2
 
-disp_sq_avg_sampled /= len(t0_iter_list)
-Dimension = 3
-
 fit_params, fit_cov = curve_fit(f, t_shortened, disp_sq_avg_sampled)
 
 print('Fitted parameters: m = {:.4e}, c = {:.4e}'.format(fit_params[0], fit_params[1]))
 print('Diffusion coefficient: D = {:.4e}'.format(fit_params[0]/(2*Dimension)))
 print('or, approximately D = {:.4f}'.format(fit_params[0]/(2*Dimension)))
 
+D_measured = fit_params[0]/(2*Dimension)
 fitline = f(t_shortened, *fit_params)
 
 plt.figure(tight_layout=True)
@@ -126,3 +125,6 @@ plt.ylabel(r'$\langle MSD^2 \rangle$', fontsize=18)
 plt.title(r"$D_{{total}} = {:.4f}$".format(fit_params[0]/(2*Dimension)))
 plt.legend(fontsize=14)
 plt.savefig('plots/D_total_l.pdf')   
+ 
+print(' D perp = {:.4f}'.format(D_measured - D_para))
+
