c6f8e04a75cf605b4e86a0da0c97dee10af45448
mconnor3565 <159495867+mconnor3565@users.noreply.github.com>
2024-04-30 17:58:47 -0400

    Add files via upload

--- c_code/get_perp.py
+++ c_code/get_perp.py
@@ -44,59 +44,89 @@ with open('disp_para.txt', 'w') as f:
         p = np.dot(h_vector, d)
         f.write("{} {} {}\n".format(t[i], p, p**2)) 
 
+
+#getting the perp displacement   
+
+with open('disp_perp.txt', 'w') as k:
+    k.write('#t disp perp\n')
+    for i in range(len(t)):
+        h_vector = np.array([f_vector[i,0], f_vector[i,1], f_vector[i,2]])
+        hx = h_vector[0]
+        hy = h_vector[1]
+        hz = h_vector[2]
+
+        if hx != 0 :
+            f = np.array([-(hy + hz)/hx,1 ,1]) 
+        elif hx == 0 and hy != 0 :
+            f = np.array([1, -(hz)/hy ,1])
+        elif hx == 0 and hy == 0 :
+            f = np.array([1,0 ,0])
+
+        f = f/np.linalg.norm(f)
+
+
+        f_prime = np.cross(h_vector,f)/np.linalg.norm(np.cross(h_vector, f))
+
+        dy = np.dot(f, disp_vector[i] )
+
+        dz = np.dot(f_prime, disp_vector[i])
+
+      
+
+        k.write("{} {} {}\n".format(t[i], dy, dz)) 
+
+
+
 #windowing average 
-t, p, p2 = np.loadtxt('disp_para.txt', unpack=True)
+t, dy ,dz = np.loadtxt('disp_perp.txt', unpack=True)
 
 t_max = len(t)
 sample_window_fraction = 0.02
-sample_window = int(t_max * sample_window_fraction)
-print("Total iterations: {}".format(t_max))
-print("Sample window: {} iterations".format(sample_window))
+sampling_window = int(t_max * sample_window_fraction)
 
-p_sq_avg_sampled = np.zeros(sample_window)
-t0_iter_list = np.arange(0, t_max-sample_window)
-print("Samples taken: {}".format(len(t0_iter_list)))
+t0_iter_list = np.arange(0, t_max-sampling_window)
+t_shortened = t[:sampling_window]
 
-t_shortened = t[:sample_window]
+disp_sq_avg_sampled= np.zeros(len(t_shortened))
 for t0_i in t0_iter_list:
-    init_p = p[t0_i]
-    p_sq_avg_sampled+= (p[t0_i:t0_i+sample_window]-init_p)**2
+    init_y = dy[t0_i]
+    init_z = dz[t0_i]
+    disp_sq_avg_sampled += (dy[t0_i:t0_i+sampling_window] - init_y)**2 + (dz[t0_i:t0_i+sampling_window] - init_z)**2
 
-    # print(init_p)
+disp_sq_avg_sampled /= len(t0_iter_list)
 
-with open("window_displacement_para.dat", "w") as file:
+
+with open("window_displacement_perp.dat", "w") as file:
     for t_i in range(len(t_shortened)):
-        file.write("{}\t{}\n".format(t_shortened[t_i], p_sq_avg_sampled[t_i]))
+        file.write("{}\t{}\n".format(t_shortened[t_i], disp_sq_avg_sampled[t_i]))
 
 
 #getting D parallel 
 def f(x, m, c):
     return m*x + c
 
-p_sq_avg_sampled /= len(t0_iter_list)
-Dimension = 1
-fit_params, fit_cov = curve_fit(f, t_shortened, p_sq_avg_sampled)
+Dimension = 2
+fit_params, fit_cov = curve_fit(f, t_shortened, disp_sq_avg_sampled)
 print('Fitted parameters: m = {:.4e}, c = {:.4e}'.format(fit_params[0], fit_params[1]))
 print('Diffusion coefficient: D = {:.4e}'.format(fit_params[0]/(2*Dimension)))
 print('or, approximately D = {:.4f}'.format(fit_params[0]/(2*Dimension)))
 
-D_para = fit_params[0]/(2*Dimension)
 fitline = f(t_shortened, *fit_params)
 
 p, D_0, D_rot_0, D_perp, D_par, D_rot = np.loadtxt("D_info.txt", unpack=True) 
 plt.figure(tight_layout=True)
-plt.plot(t_shortened, p_sq_avg_sampled, 'k-', label='Data')
+plt.plot(t_shortened, disp_sq_avg_sampled, 'k-', label='Data')
 plt.plot(t_shortened, fitline, 'r--', label='Fit')
 plt.xlabel(r'$t/\tau$', fontsize=18)
 plt.ylabel(r'$\langle MSD_{parallel}^2 \rangle$', fontsize=18)
-plt.title(r"$D_{{para}} = {:.4e}$".format(D_0*fit_params[0]/(2*Dimension)))
+plt.title(r"$D_{{perp}} = {:.4e}$".format(D_0*fit_params[0]/(2*Dimension)))
 plt.legend(fontsize=14)
-plt.savefig('D_parallel_c.pdf')
+plt.savefig('D_perp_c.pdf')
 
 # #getting D total
 # t, p_sq = np.loadtxt("window_displacement.dat", unpack=True)
 
-# Dimension= 1
+# Dimension= 2
 
 # def f(x, m, c):
 #     return m * x + c
